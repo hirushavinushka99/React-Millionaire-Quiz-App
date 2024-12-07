fac365e05ebc644f0a1af0511a08c84802702651
Hirusha De Silva <169069562+hirushavinushka99@users.noreply.github.com>
2024-12-07 17:11:26 +0530

    c5

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+  return (
+    <div className="app">
+        <div className="main">main</div>
+        <div className="pyramid">money</div>
+    </div>
+  );
+}
+
+export default App;
