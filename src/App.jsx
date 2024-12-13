f034e9bbb342a45f1245ee1299a161906c69da6f
Hirusha De Silva <169069562+hirushavinushka99@users.noreply.github.com>
2024-12-13 13:10:54 +0530

    c13

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import { useState } from "react";
 import "./app.css";
+import Trivia from "./components/Trivia";
+
+
+
+
 
 function App() {
   const [questionNumber, setQuestionNumber] = useState(5)
@@ -26,7 +31,9 @@ function App() {
           <div className="top">
             <div className="timer">30</div>
           </div>
-          <div className="bottom">question and answers</div>
+          <div className="bottom">
+            <Trivia />
+          </div>
         </div>
         <div className="pyramid">
           <ul className="moneyList">
