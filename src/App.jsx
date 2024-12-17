a6fa7b58ba460ae771431f998a017d085511190e
Hirusha De Silva <169069562+hirushavinushka99@users.noreply.github.com>
2024-12-17 20:18:10 +0530

    c21

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Trivia from "./components/Trivia";
 
 function App() {
   const [questionNumber, setQuestionNumber] = useState(1)
-  const [timeOut, setTimeOut] = useState(false);
+  const [stop, setStop] = useState(false);
 
   const data = [
     {
@@ -100,7 +100,7 @@ function App() {
           </div>
           <div className="bottom">
             <Trivia data={data} 
-            setTimeOut={setTimeOut} 
+            setStop={setStop} 
             questionNumber={questionNumber}
             setQuestionNumber={setQuestionNumber}
             />
