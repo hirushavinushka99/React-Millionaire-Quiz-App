44faf7d0cda895339f6d9fd683859ae222318697
Hirusha De Silva <169069562+hirushavinushka99@users.noreply.github.com>
2024-12-18 21:06:57 +0530

    c22

--- src/components/Trivia.jsx
+++ src/components/Trivia.jsx
@@ -14,14 +14,30 @@ export default function Trivia({
     setQuestion(data[questionNumber - 1]);
   },[data, questionNumber]);
 
+  const delay = (duration, callback) =>{
+    setTimeout(()=>{
+      callback();
+    }, duration);
+  };
+
   const handleClick = (a) => {
     setSelectedAnswer(a);
     setClassName("answer active");
-    setTimeout(()=>{
+    delay(3000, () => 
       setClassName(a.correct ? "answer correct" : "answer wrong")
+  );
+    delay(6000, () => 
+    {
+      if(a.correct){
+        setQuestionNumber((prev) => prev +1);
+        setSelectedAnswer(null);
+      } else{
+        setStop(true);
+      }
+    });
+};
 
-    },3000)
-  };
+
 
   return (
     <div className="trivia">
