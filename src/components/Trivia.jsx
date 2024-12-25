818f81a394ecdfbc595720255c0326483dec1e35
Hirusha De Silva <169069562+hirushavinushka99@users.noreply.github.com>
2024-12-25 14:23:49 +0530

    c28

--- src/components/Trivia.jsx
+++ src/components/Trivia.jsx
@@ -1,4 +1,8 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
+import useSound from "use-sound";
+import play from "../assets/play.wav"
+import correct from "../assets/correct.wav"
+import wrong from "../assets/wrong.wav"
 
 export default function Trivia({
   data,
@@ -9,6 +13,13 @@ export default function Trivia({
   const [question, setQuestion] = useState(null);
   const [selectedAnswer, setSelectedAnswer] = useState(null);
   const [className, setClassName] = useState("answer");
+  const [letsPlay] = useSound(play)
+  const [correctAnswer] = useSound(correct)
+  const [wrongAnswer] = useSound(wrong)
+
+  useEffect(()=>{
+    letsPlay();
+  }, [letsPlay]);
 
   useEffect(()=>{
     setQuestion(data[questionNumber - 1]);
@@ -26,18 +37,22 @@ export default function Trivia({
     delay(3000, () => 
       setClassName(a.correct ? "answer correct" : "answer wrong")
   );
-    delay(6000, () => 
+    delay(5000, () => 
     {
       if(a.correct){
-        setQuestionNumber((prev) => prev +1);
-        setSelectedAnswer(null);
+        correctAnswer();
+        delay(1000,()=>{
+          setQuestionNumber((prev) => prev + 1);
+          setSelectedAnswer(null);
+        });
       } else{
-        setStop(true);
+        wrongAnswer();
+        delay(1000,()=>{
+          setStop(true);
+        });
       }
     });
 };
-
-
 
   return (
     <div className="trivia">
