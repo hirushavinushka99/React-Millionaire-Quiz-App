7d49d387ba261194f405dd011dcbe98b1892405c
Hirusha De Silva <169069562+hirushavinushka99@users.noreply.github.com>
2024-12-27 14:31:03 +0530

    c31

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -1,8 +1,18 @@
-export default function Start() {
+import { useRef } from "react"
+
+export default function Start(setUsername) {
+    const inputRef = useRef();
+    const handleClick = () => {
+        inputRef.current.value && setUsername(inputRef.current.value);
+    };
   return (
     <div className="start">
-        <input placeholder="Enter your name" className="startInput"/>
-        <button className="startButton">Start</button>    
+        <input 
+        placeholder="Enter your name" 
+        className="startInput" 
+        ref={inputRef}
+        />
+        <button className="startButton" onClick={handleClick}>Start</button>    
     </div>
   )
 }
